b7dbcd4069486c22c40425ab07a097014b94bc97
khandaitpiyush <khandaitpiyush@gmail.com>
2026-05-06 09:42:26 +0530

    New Origins Allowed

--- backend/server.js
+++ backend/server.js
@@ -68,6 +68,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "https://pass-it-on-piyush.netlify.app",
+  "https://pass-it-on-piyushh.netlify.app",
 ];
 
 /* -------------------------------------------------------------------------- */
